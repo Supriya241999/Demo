8c13c548967cb9a9800fdadcda52af7d62a39dfa
Supriya241999 <suprriya25@gmail.com>
2024-10-06 18:49:35 +0530

    Update calculator.go

--- Multistage/calculator.go
+++ Multistage/calculator.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi Abhishek.Veeramalla, I am a calculator app ....")
+	fmt.Println("Hi Supriya, I am a calculator app ....")
 
 	for {
 		// Read input from the user
