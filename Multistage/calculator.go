0ff6fa34443e4c64f374ec852b0cf8d3b8e2c2a3
Supriya241999 <suprriya25@gmail.com>
2024-10-06 19:10:01 +0530

    Update calculator.go

--- Multistage/calculator.go
+++ Multistage/calculator.go
@@ -1,66 +1,52 @@
-package main
+import os
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+def cls():
+    os.system(['clear','cls'][os.name == 'nt'])
 
-func main() {
-	fmt.Println("Hi Supriya, I am a calculator app ....")
+# now, to clear the screen
+cls()
 
-	for {
-		// Read input from the user
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter any calculation (Example: 1 + 2 (or) 2 * 5 -> Please maintain spaces as shown in example): ")
-		text, _ := reader.ReadString('\n')
 
-		// Trim the newline character from the input
-		text = strings.TrimSpace(text)
+loop = 1
 
-		// Check if the user entered "exit" to quit the program
-		if text == "exit" {
-			break
-		}
+#this variable holds the user's choice in the menu:
 
-		// Split the input into two parts: the left operand and the right operand
-		parts := strings.Split(text, " ")
-		if len(parts) != 3 {
-			fmt.Println("Invalid input. Try again.")
-			continue
-		}
+choice = 0
 
-		// Convert the operands to integers
-		left, err := strconv.Atoi(parts[0])
-		if err != nil {
-			fmt.Println("Invalid input. Try again.")
-			continue
-		}
-		right, err := strconv.Atoi(parts[2])
-		if err != nil {
-			fmt.Println("Invalid input. Try again.")
-			continue
-		}
+while loop == 1:
 
-		// Perform the calculation based on the operator
-		var result int
-		switch parts[1] {
-		case "+":
-			result = left + right
-		case "-":
-			result = left - right
-		case "*":
-			result = left * right
-		case "/":
-			result = left / right
-		default:
-			fmt.Println("Invalid operator. Try again.")
-			continue
-		}
 
-		// Print the result
-		fmt.Printf("Result: %d\n", result)
-	}
-}
+    #print what options you have
+    print "Welcome to calculator.py"
+    print " "
+    print "Main Menu"
+    print "1) Addition"
+    print "2) Subtraction"
+
+    print "3) Multiplication"
+
+    print "4) Division"
+    print "5) Quit calculator.py"
+    print " "
+
+    choice = input("Choose your option: ")
+    if choice == 1:
+        add1 = input("Add this: ")
+        add2 = input("to this: ")
+        print add1, "+", add2, "=", add1 + add2
+    elif choice == 2:
+        sub2 = input("Subtract this: ")
+        sub1 = input("from this: ")
+        print sub1, "-", sub2, "=", sub1 - sub2
+    elif choice == 3:
+        mul1 = input("Multiply this: ")
+        mul2 = input("with this: ")
+        print mul1, "*", mul2, "=", mul1 * mul2
+    elif choice == 4:
+        div1 = input("Divide this: ")
+        div2 = input("by this: ")
+        print div1, "/", div2, "=", div1 / div2
+    elif choice == 5:
+        loop = 0
+
+print "Thankyou for using calculator.py!"
